4ffbabf02eb1c1fbcba03120cdf188e1ea837c67
999 <hyeon@imweb.me>
2022-05-18 08:54:59 +0900

    feat: document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/app.css';
-import '../styles/globals.scss';
-import '../public/fonts/fontello/css/fontello.css';
+import '~/stlyes/bootstrap/tailwind.scss';
+import '~/styles/bootstrap/globals.scss';
+import '~/styles/app.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />;
