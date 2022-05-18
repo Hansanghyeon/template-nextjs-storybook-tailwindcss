63c2b306a953d9039dea355eaa33d1b9858e46f3
999 <hyeon@imweb.me>
2022-05-18 09:00:54 +0900

    feat: public 폴더위치

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = {
   stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ['../public'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
