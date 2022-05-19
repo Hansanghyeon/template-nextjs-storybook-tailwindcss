2f63eedb73924cb83ec1371e26f3757d4cf1f792
Hansanghyeon <999@hyeon.pro>
2022-05-20 07:59:40 +0900

    feat: React 18, _app style path error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '~/stlyes/bootstrap/tailwind.scss';
+import '~/styles/bootstrap/tailwind.scss';
 import '~/styles/bootstrap/globals.scss';
 import '~/styles/app.css';
 import type { AppProps } from 'next/app';
